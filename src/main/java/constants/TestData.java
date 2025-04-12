21e3711119afcbe9085d6ec17b5ea987aaf9dade
Aleksandr Belimov <vontas@mail.ru>
2025-04-12 21:12:04 +0300

    add allure report

---
+++ src/main/java/constants/TestData.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class TestData {
+    public static final String BASE_URI = "https://stellarburgers.nomoreparties.site";
+    public static final String LOGIN = "testuser_";
+    public static final String MAIL = "@mails.ru";
+    public static final String PASSWORD = "agswer";
+    public static final String NAME = "Александр";
+    public static final String INCORRECT_PASSWORD = "qwe";
+    public static final String PASSWORD_INPUT_ERROR = "Некорректный пароль";
+}
